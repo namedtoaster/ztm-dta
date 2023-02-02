f31d24687fec5a9681e77eaa0ca43be6d288df51
Dave Campbell <davecampbell@Daves-MacBook-Pro.local>
2023-02-01 17:43:02 -0800

    create graph data structure

---
+++ datastructures/graphs/graph.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+//     2-----0
+//   /  \
+// 1------3
+
+// ways to create a graph (JS)
+// 1. edge list
+// const graph = [[0,2],[2,3],[2,1],[1,3]];
+// just shows the connections
+
+// 2. adjacency list
+// index of the data structure is the node,
+// and the value is the connections
+// const graph = [[2], [2,3], [0,1,3], [2,1]];
+
+// 3. adjacency matrix
+// 0s and 1s to indicate if y (row)
+// has connection to x (col)
+// const graph = [
+// [0,0,1,0],
+// [0,0,1,1],
+// [1,1,0,1],
+// [0,1,1,0]
+// ]
+
+int main() {
+}
